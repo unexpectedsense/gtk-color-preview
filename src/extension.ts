5417afd5646cda155c81071f38759654658dfc89
unexpectedsense <javierbaldo@gmail.com>
2026-02-18 20:54:48 -0300

    rename command

--- src/extension.ts
+++ src/extension.ts
@@ -13,10 +13,10 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('gtk-color-preview.helloWorld', () => {
+	const disposable = vscode.commands.registerCommand('gtk-color-preview.refresh', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from gtk-color-preview!');
+		vscode.window.showInformationMessage('GTK Color Preview: Updated previews!');
 	});
 
 	context.subscriptions.push(disposable);
